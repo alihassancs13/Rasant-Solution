4f21ce08c532c044802e553723c2ca7766c27558
Shujaat Arbab <shujaatarbab11@gmail.com>
2026-06-17 12:41:36 +0500

    created new file for fontawesome

---
+++ frontend/src/fontAwesomeIcons/icon.js
@@ -0,0 +1,38 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faChevronLeft,
+  faChevronRight,
+  faArrowRight,
+  faRocket,
+  faHandshake,
+  faBolt,
+  faStar,
+  faMobileScreen,
+  faServer,
+} from '@fortawesome/free-solid-svg-icons'
+import {
+  faReact,
+  faNodeJs,
+  faPython,
+  faAws,
+  faDocker,
+  faGoogle,
+} from '@fortawesome/free-brands-svg-icons'
+
+library.add(
+  faChevronLeft,
+  faChevronRight,
+  faArrowRight,
+  faRocket,
+  faHandshake,
+  faBolt,
+  faStar,
+  faMobileScreen,
+  faServer,
+  faReact,
+  faNodeJs,
+  faPython,
+  faAws,
+  faDocker,
+  faGoogle
+)
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 import './assets/main.css'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import '../icon.js' 
+
 const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router)
 app.mount('#app')
